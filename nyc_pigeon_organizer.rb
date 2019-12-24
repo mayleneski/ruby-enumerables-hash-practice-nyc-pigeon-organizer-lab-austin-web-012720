e91c7d5640c4e8ec811d360be53af831888b6678
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-24 02:22:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,25 +2,29 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   
-  #iterate through the keys (:color, :gender, and :lives) first
-  #line 7 block parameters returns the keys inside of the value of :color, :gender, :lives 
+#iterate through the keys (:color, :gender, and :lives) first
+#line 7 block parameters returns the keys inside of the value of :color, :gender, :lives 
   
   data.each do |first_order_keys, value|
   
-  #This block will iterate over the second order keys and return the elements in the arrays (that are the values) 
+#This block will iterate over the second order keys and return the elements in the arrays (that are the values) 
     
     value.each do |second_order_keys, array|
       
-  #Then we want to iterate over the array
+#Then we want to iterate over the array
   
       array.each do |name|
         if pigeon_list[name] == nil
           pigeon_list[name] = {}
-        end 
+        end
+        
+ #The next line of code will assign the first order keys inside the name value hashes to an empty arrays
+ 
         if pigeon_list[name][first_order_keys] == nil
           pigeon_list[name][first_order_keys] = []
         end
-        #Now we are going to push the values from the second order keys into the empty arrays
+        
+  #Now we are going to push the values from the second order keys into the empty arrays
         
         pigeon_list[name][first_order_keys].push(second_order_keys.to_s)
       end  
